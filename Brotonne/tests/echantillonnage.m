7add0242f6c3520fb3b4f21ce997d4175bf0ce66
Lucas <lucas.erard@insa-rouen.fr>
2015-01-05 16:57:08 +0100

    Mieux

--- Brotonne/tests/echantillonnage.m
+++ Brotonne/tests/echantillonnage.m
@@ -2,8 +2,8 @@ function v = echantillonnage(image,pas)
 [n,m]=size(image);
 v=[];
 for i=1:pas:n
-    for j=1:pas:m
-	v=[v;i j image(i,i)];
+    for j=2:pas:m
+	v=[v;i j image(i,j)];
     end
 end
 end
